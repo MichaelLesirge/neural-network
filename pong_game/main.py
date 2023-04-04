d50b3c0ffede8ed48648f9efb3e5e1d1da71b9ed
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-04-03 17:16:17 -0700

    make game a class

--- pong_game/main.py
+++ pong_game/main.py
@@ -5,9 +5,7 @@ from utils import to_rect_relitive_points, rect_centered_point
 
 
 def make_screen_size(size_px: int, aspect_ration: float, horizontal: bool = True):
-    return int(size_px * ((aspect_ration * (horizontal)) or 1)), int(size_px * ((aspect_ration * (not horizontal)) or 1))
-
-# def does_ball_collide(ball: , paddle: pygame.rect.Rect)
+    return int(size_px * ((aspect_ration * (horizontal)) or 1)), int(size_px * ((aspect_ration * (not horizontal)) or 1))    
 
 class GameConstants:
     WINDOW_NAME = "Pong Game"
@@ -33,94 +31,100 @@ class PaddleConstants:
     PADDLE_SIZE = 0.01, 0.2
 
 
-def main():
-    pygame.init()
-    pygame.display.set_caption(GameConstants.WINDOW_NAME)
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode(
+            make_screen_size(size_px=400, aspect_ration=16/10, horizontal=True)
+        )
+        pygame.display.set_caption(GameConstants.WINDOW_NAME)
+        
+        self.screen_rect = self.screen.get_rect()
 
-    screen = pygame.display.set_mode(
-        make_screen_size(size_px=400, aspect_ration=16/10, horizontal=True)
-    )
+        self.clock = pygame.time.Clock()
+        
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
+
+        self.ball = Ball(
+            BallConstants.SIZE, to_rect_relitive_points(self.screen_rect, BallConstants.START_LOCATION),
+            BallConstants.START_SLOPE, BallConstants.START_VELOCITY, BallConstants.MAX_VELOCITY
+        )
+
+        self.player1 = HumanPaddle(
+            to_rect_relitive_points(self.screen_rect, PaddleConstants.START_LOCATION, reverse_x=False),
+            to_rect_relitive_points(self.screen_rect, PaddleConstants.PADDLE_SIZE),
+            pygame.K_q, pygame.K_a
+        )
+
+        self.player2 = HumanPaddle(
+            to_rect_relitive_points(self.screen_rect, PaddleConstants.START_LOCATION, reverse_x=True),
+            to_rect_relitive_points(self.screen_rect, PaddleConstants.PADDLE_SIZE),
+            pygame.K_p, pygame.K_l
+        )
+
+        self.all_sprites = pygame.sprite.Group(self.ball, self.player1, self.player2)  # type: ignore
+
+    def run(self) -> None:
+        running = True
+        while running:
+            self.update_screen()
+            if pygame.event.get(pygame.QUIT):
+                running = False
+        pygame.quit()
     
-    screen_rect = screen.get_rect()
+    def update_screen(self) -> None:
+        self.screen.fill("black")
+        pygame.draw.line(self.screen, "white",
+                        to_rect_relitive_points(self.screen_rect, GameConstants.LINE_LOCATION),
+                        to_rect_relitive_points(self.screen_rect, GameConstants.LINE_LOCATION, reverse_y=True))
 
-    clock = pygame.time.Clock()
-    
-    font = pygame.font.Font('freesansbold.ttf', 32)
-
-    ball = Ball(
-        BallConstants.SIZE, to_rect_relitive_points(screen_rect, BallConstants.START_LOCATION),
-        BallConstants.START_SLOPE, BallConstants.START_VELOCITY, BallConstants.MAX_VELOCITY
-    )
-
-    player1 = HumanPaddle(
-        to_rect_relitive_points(screen_rect, PaddleConstants.START_LOCATION, reverse_x=False),
-        to_rect_relitive_points(screen_rect, PaddleConstants.PADDLE_SIZE),
-        pygame.K_q, pygame.K_a
-    )
-
-    player2 = HumanPaddle(
-        to_rect_relitive_points(screen_rect, PaddleConstants.START_LOCATION, reverse_x=True),
-        to_rect_relitive_points(screen_rect, PaddleConstants.PADDLE_SIZE),
-        pygame.K_p, pygame.K_l
-    )
-
-    all_sprites = pygame.sprite.Group(ball, player1, player2)  # type: ignore
-
-    running = True
-    while running:
-        screen.fill("black")
-        pygame.draw.line(screen, "white",
-                        to_rect_relitive_points(screen_rect, GameConstants.LINE_LOCATION),
-                        to_rect_relitive_points(screen_rect, GameConstants.LINE_LOCATION, reverse_y=True))
-
-        if pygame.event.get(pygame.QUIT):
-            running = False
-
-        all_sprites.update()
+        self.all_sprites.update()
 
         # prevent paddles from leaving screen
-        player1.rect.clamp_ip(screen_rect)
-        player2.rect.clamp_ip(screen_rect)
+        self.player1.rect.clamp_ip(self.screen_rect)
+        self.player2.rect.clamp_ip(self.screen_rect)
 
-        if ball.rect.collideobjects((player1.rect, player2.rect)):
+        if self.ball.rect.collideobjects((self.player1.rect, self.player2.rect)):
             # ball hit a paddle
-            ball.bounce_x()
-            ball.add_velocity(BallConstants.BOUNCE_VELOCITY_INCREACE)
+            self.ball.bounce_x()
+            self.ball.add_velocity(BallConstants.BOUNCE_VELOCITY_INCREACE)
 
-        elif ball.rect.right < screen_rect.left:
+        elif self.ball.rect.right < self.screen_rect.left:
             # Went over the left side of screen, player 2 wins
-            ball.set_motion(
-                to_rect_relitive_points(screen_rect, BallConstants.START_LOCATION, reverse_x=True),
+            self.ball.set_motion(
+                to_rect_relitive_points(self.screen_rect, BallConstants.START_LOCATION, reverse_x=True),
                 BallConstants.START_SLOPE, BallConstants.START_VELOCITY
             )
-            ball.bounce_x()
-            player2.add_score()
+            self.ball.bounce_x()
+            self.player2.add_score()
             
-        elif ball.rect.left > screen_rect.right:
+        elif self.ball.rect.left > self.screen_rect.right:
             # Went over the right side of screen, player 1 wins
-            ball.set_motion(
-                to_rect_relitive_points(screen_rect, BallConstants.START_LOCATION, reverse_x=False),
+            self.ball.set_motion(
+                to_rect_relitive_points(self.screen_rect, BallConstants.START_LOCATION, reverse_x=False),
                 BallConstants.START_SLOPE, BallConstants.START_VELOCITY
             )
-            player1.add_score()
+            self.player1.add_score()
         
-        if ball.rect.top < screen_rect.top or ball.rect.bottom > screen_rect.bottom:
+        if self.ball.rect.top < self.screen_rect.top or self.ball.rect.bottom > self.screen_rect.bottom:
             # Hit top or buttom
-            ball.bounce_y()
+            self.ball.bounce_y()
             
-        player1_score_font = font.render(str(player1.score), True, "white")
-        screen.blit(player1_score_font, rect_centered_point(player1_score_font.get_rect(), to_rect_relitive_points(screen_rect, GameConstants.SCORE_LOCATION, reverse_x=False)))
+        player1_score_font = self.font.render(str(self.player1.score), True, "white")
+        self.screen.blit(player1_score_font, rect_centered_point(player1_score_font.get_rect(), to_rect_relitive_points(self.screen_rect, GameConstants.SCORE_LOCATION, reverse_x=False)))
         
-        player2_score_font = font.render(str(player2.score), True, "white")
-        screen.blit(player2_score_font, rect_centered_point(player2_score_font.get_rect(), to_rect_relitive_points(screen_rect, GameConstants.SCORE_LOCATION, reverse_x=True)))
+        player2_score_font = self.font.render(str(self.player2.score), True, "white")
+        self.screen.blit(player2_score_font, rect_centered_point(player2_score_font.get_rect(), to_rect_relitive_points(self.screen_rect, GameConstants.SCORE_LOCATION, reverse_x=True)))
 
-        all_sprites.draw(screen)
+        self.all_sprites.draw(self.screen)
         pygame.display.update() 
 
-        clock.tick(GameConstants.FRAMERATE)
+        self.clock.tick(GameConstants.FRAMERATE)
 
-    pygame.quit()
-
+def main() -> None:
+    game = Game()
+    game.run()
 
 if __name__ == "__main__":
     main()
