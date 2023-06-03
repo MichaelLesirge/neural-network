089ef8335359fdebee2dcdc259b846f777c0511e
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-06-02 19:12:17 -0700

    give it a test

--- neural_network/testing/first_nerual_network.py
+++ neural_network/testing/first_nerual_network.py
@@ -1,10 +1,11 @@
 import numpy as np
+from random import randint
 
 def sigmoid(x):
     return 1 / (1-np.exp(-x))
 
 class NeuralNetwork:
-    def __init__(self, layer_sizes: list[int], activation_funtion) -> None:
+    def __init__(self, layer_sizes: list[int], activation_funtion = sigmoid) -> None:
         self.activations = [np.zeros((num_rows, 1)) for num_rows in layer_sizes]
 
         self.biases = [np.zeros((num_rows, 1)) for num_rows in layer_sizes[1:]]
@@ -12,7 +13,10 @@ class NeuralNetwork:
         weights_shapes = [(num_rows, num_cols) for num_rows, num_cols in zip(layer_sizes[1:], layer_sizes[:-1])]     
         self.weights = [np.random.standard_normal(shape) for shape in weights_shapes]
         
-        self.activation_funtion = activation_funtion
+        self.activation_funtion = np.vectorize(activation_funtion)
+    
+    def randomly_change_weight(self):
+        pass
     
     def feed_forward(self, inputs):
         self.activations[0] = inputs
@@ -35,15 +39,50 @@ class NeuralNetwork:
         print("\nWeights:")
         for w in self.weights: print(w, "\n")
         
+def error(output: np.ndarray, correct: np.ndarray) -> float:
+    return ((output - correct) ** 2).mean()
+
+def make_input(n: int, *, size: int = 10):
+    inputs = np.zeros((10, 1))
+    inputs[n % size] = 1
+    return inputs
 
 def main() -> None:
-    network = NeuralNetwork([10, 20, 10], sigmoid)
     
-    inputs = np.array([[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]])
+    good_enough_error = 0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000001
+    print(good_enough_error)
+    best_network = NeuralNetwork([0])
+    min_error = float("inf")
     
-    output = network.feed_forward(inputs)
-    difference = (output - inputs) ** 2
-    print(np.sum(difference))
+    while min_error > good_enough_error:
+        network = NeuralNetwork([10, 20, 10], sigmoid)
+    
+        inputs = make_input(1) 
+    
+        outputs = network.feed_forward(inputs)
+    
+        cur_error = error(outputs, inputs)
+        
+        if cur_error < min_error:
+            min_error = cur_error
+            print("Best min error:", min_error)
+    
+    print()
+    
+    while True:
+        inputs = make_input(int(input("Number: "))) 
+    
+        outputs = best_network.feed_forward(inputs)
+        
+        print(outputs)
+
+        best_network.output_activation()
+        cur_error = error(outputs, inputs)
+        
+        print("Error: ", cur_error)
+        print("Guess: ", np.argmax(outputs))
+        
+        print()
      
 if __name__ == "__main__":
     main()
