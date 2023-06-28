3cd00b86636a86c26f716ffb9654090054b09d70
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-06-27 22:18:11 -0700

    change back example steps

--- testing/example_steps.py
+++ testing/example_steps.py
@@ -5,42 +5,57 @@ import numpy as np
 
 inputs = np.array([1, 2])
 
-weights = np.array([[3, 4], [5, 6], [7, 8]])
+weights = np.array([[3, 4, 5], [6, 7, 8]])
 
 biases = np.array([9, 10, 11])
 
 def relu(x): return np.maximum(x, 0)
-activation = relu
+activation_function = relu
 
 actual = [10, 20, 30]
 
 def mse(true, actual): return np.mean(np.square(true - actual))
 error_function = mse
 
-learning_rate = 0.001
-
-# [1, 2] * [[3, 4], [5, 6], [7, 8]]
-# [1*3 + 2*4, 1*5 + 2*6, 1*7 + 2*8]
-# [3 + 8, 5 + 12, 7 + 16]
-# [11, 17, 23]
-prediction_weights = np.dot(weights, inputs)
-print("inputs * weights =", prediction_weights)
-print(f"{inputs.tolist()} * {weights.tolist()} =", prediction_weights)
+# [1, 2] * [[3, 4, 5], [6, 7, 8]]
+# [1*3 + 2*6, 1*4 + 2*7, 1*5 + 2*8]
+# [3 + 12, 4 + 14, 5 + 16]
+# [15, 18, 21]
+prediction_step_1 = np.dot(inputs, weights)
+print("inputs * weights =", prediction_step_1)
+print(f"{inputs.tolist()} * {weights.tolist()} =", prediction_step_1)
 print()
 
-prediction_biases = prediction_weights + biases
-print("inputs * weights + biases =", prediction_biases)
-print(f"{prediction_weights.tolist()} + {biases.tolist()} =", prediction_biases)
+# [15, 18, 22] + [15, 18, 21]
+# [15 + 9, 18 + 10, 21 + 11]
+# [24, 28, 32]
+prediction_step_2 = prediction_step_1 + biases
+print("inputs * weights + biases =", prediction_step_2)
+print(f"{prediction_step_1.tolist()} + {biases.tolist()} =", prediction_step_2.tolist())
 print()
 
-prediction_activation = relu(prediction_biases)
-print("activation(inputs * weights + biases) =", prediction_activation)
-print(f"{activation.__name__}({prediction_biases.tolist()}) =", prediction_biases)
+
+# relu([24, 28, 32])
+# [max(24, 0), max(28, 0), max(32, 0)])
+# [24, 28, 32]
+prediction_step_3 = activation_function(prediction_step_2)
+print("activation(inputs * weights + biases) =", prediction_step_3)
+print(f"{activation_function.__name__}({prediction_step_2.tolist()}) =", prediction_step_3.tolist())
 print()
 
-error = mse(actual, prediction_activation)
+# mean(([10, 20, 30] - [24, 28, 32]) ^ 2)
+# mean(([10-24, 20-28, 30-32]) ^ 2)
+# mean(([-14, -8, -2]) ** 2)
+# mean(([-14^2, -8^2, -2^2]))
+# mean(([196, 64, 4]))
+
+# sum([196, 64, 4]) / len([196, 64, 4])
+# 264 / 3
+# 88
+
+error = error_function(actual, prediction_step_3)
 print("error(activation(inputs * weights + biases)) =", error)
-print(f"{error_function.__name__}({prediction_activation.tolist()}) =", error)
+print(f"{error_function.__name__}({prediction_step_3.tolist()}) =", error)
 print()
 
 
