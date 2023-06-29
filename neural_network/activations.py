fix softmax activation to prevent numbers getting to high

neural_network/activations.py
@@ -103,7 +103,8 @@ def softmax(x):
     Softmax.
     Good for output layer as it makes sum of 1
     """
-    return np.exp(x) / np.sum(np.exp(x))
+    exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+    return exp_x / np.sum(exp_x, axis=1, keepdims=True)
 
 
 def swish(x):