a6b90bf77babea0d05194410138516fe59de5558
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-07-09 13:31:04 -0700

    fix deprecation warning

--- testing/graident_decent.py
+++ testing/graident_decent.py
@@ -2,27 +2,20 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-def y_func(x): return 0.1 * x ** 2
-# def y_func(x): return np.sin(0.5 * x) + 0.3 * np.sin(2 * x) + 0.2 * x
+"""Gradient descent with Linear Approximations"""
 
-def mean_squared_error(predicted, target):
-    return np.mean(np.square(predicted - target))    
+# def y_func(x): return 0.1 * x ** 2
+def y_func(x): return (np.sin(0.5 * x) + 0.3 * np.sin(2 * x) + 0.2 * x) + 2.00930389075277116
 
-x_min_max, step = 10, 0.1
+x_min_max, step = 10, 0.001
 x_data = np.arange(-x_min_max, x_min_max + step, step)
+
 y_data = y_func(x_data)
 
 y_target = 0
 
-"""
-TODO:
-Fix  MatplotlibDeprecationWarning: dots.set_data(point) by using scatter or something, not a hacky way
-Add percentage done on animation printout
-Add error threshold use
-"""
-
-learning_rate = 0.01
-error_threshold = 0.0001
+learning_rate = 0.1
+# error_threshold = 0.00001
 
 positions_and_errors = []
 
@@ -30,11 +23,17 @@ max_iterations = 5000 + 1
 
 x = np.random.choice(x_data)
  
+def mean_squared_error(predicted, target):
+    return np.mean(np.square(predicted - target))    
+
 for i in range(max_iterations):
     y = y_func(x)
     
     error = mean_squared_error(y, y_target)
     positions_and_errors.append(((x, y), error))
+    
+    # if error < error_threshold:
+    #     break
     
     error_delta = mean_squared_error(y_func(x + 0.00001), y_target)
     gradient = (error_delta - error) / 0.00001
@@ -47,6 +46,7 @@ for i in range(max_iterations):
 error_figure, error_axis = plt.subplots()
 error_figure.suptitle("Error")
 error_axis.plot([error for position, error in positions_and_errors ], label = "Error")
+error_axis.set_ylim(bottom=0)
 error_axis.legend(loc="best")
 
 figure, axis = plt.subplots()
@@ -61,16 +61,14 @@ def init():
     return (dots,)
 
 print_step = 100
-def update(n_frame):
-    global last
+def update(n_frame):         
+    (x, y), error = positions_and_errors[n_frame]
     
-    point, error = positions_and_errors[n_frame]
+    dots.set_data([x], [y])
     
-    dots.set_data(point)
-    
-    if (n_frame % print_step == 0):
+    if (n_frame % print_step == 0) or n_frame == len(positions_and_errors)-1:
         last_point, last_error = positions_and_errors[max(n_frame - print_step, 0)]
-        print(f"#{n_frame}: {point=}, {error=}. {(last_error - error) / last_error:%} better than last")
+        print(f"#{n_frame}:\tpoint=({x:g}, {y:g})\terror={error:g}\tchange={(last_error - error) / last_error:%}")
     
     return (dots,)
 
