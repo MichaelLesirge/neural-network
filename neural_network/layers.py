15d348d00ec4de4350f0c4c111ed27fa92151deb
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-07-12 00:56:38 -0700

    AAAAAaaaaaaaaaaaaaaaaaaaaaaaaa

--- neural_network/layers.py
+++ neural_network/layers.py
@@ -12,58 +12,37 @@ class Layer(ABC):
         ...
 
 class Dense(Layer):
-    def __init__(self, input_size: int, output_size: int, activation: Activation) -> None:
-
-        # Initialize weights from standard normal distribution.
-        # Each row of weights lines up with a input and each column lines up with a output
+    def __init__(self, input_size, output_size, activation):
         self.weights = np.random.randn(output_size, input_size)
+        self.biases = np.ones((output_size, 1))
 
-        # Initialize biases to 0s.
-        # One bias for each output.
-        self.biases = np.zeros(output_size, dtype=float)
-
-        # save activation function
         self.activation = activation
 
-    def forward(self, input: np.ndarray) -> np.ndarray:
+    def forward(self, input):
         self.input = input
+        return np.dot(self.weights, self.input) + self.biases
 
-        # standard forward pass of neuron with no activation
-        forward = np.dot(self.input, self.weights) + self.biases
-
-        # use activation function
-        return self.activation.func(forward)
-
-    def backward(self, output_gradient: np.ndarray, learning_rate: float) -> np.ndarray:
-        
-        # factor in gradient from activation function
-        output_gradient = self.activation.gradient(output_gradient)
-
-        # calculate gradient of input by linking up output_gradients with columns that align with that output
-        input_gradient = np.dot(self.weights.T, output_gradient)
-
-        # 
+    def backward(self, output_gradient, learning_rate):
         weights_gradient = np.dot(output_gradient, self.input.T)
-        # biases gradient stays output gradient
-        biases_gradient = output_gradient
-
-        # change 
+        
+        input_gradient = np.dot(self.weights.T, output_gradient)
+        
         self.weights -= learning_rate * weights_gradient
-        self.biases -= learning_rate * biases_gradient
+        self.biases -= learning_rate * output_gradient
         
         return input_gradient
     
 def main():
-    from activations import ReLU
-    
+    from activations import Sigmoid, ReLU
+
     l = Dense(2, 3, activation=ReLU())
     
-    l.weights = np.array([[3, 4, 5],
-                          [6, 7, 8]])
-    l.biases = np.array([9, 10, 11])
+    # l.weights = np.array([[3, 4, 5],
+    #                       [6, 7, 8]])
+    # l.biases = np.array([9, 10, 11])
     
-    # print(l.forward([1, 2]))
-    print(l.forward([[1, 2], [1, 2]]))
+    print(l.forward([[1], [2]]))
+    print(l.forward([[[1], [2]], [[1], [2]]]))
     
 if __name__ == "__main__":
     main()
