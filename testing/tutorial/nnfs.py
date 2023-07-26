39432d3776e08b79d46e0b35d9d342fe09672976
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-07-25 23:33:17 -0700

    get own implantation of making spiral data

--- testing/tutorial/nnfs.py
+++ testing/tutorial/nnfs.py
@@ -1,10 +1,21 @@
 import numpy as np
-from nnfs.datasets import vertical_data as dataset_maker
 from matplotlib import pyplot as plt
 
 # 10/10 book would recommend: https://nnfs.io/
 
-X, y = dataset_maker(samples=100, classes=3)
+def create_data(samples, classes):
+    X = np.zeros((samples*classes, 2))
+    y = np.zeros(samples*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(samples*class_number, samples*(class_number+1))
+        r = np.linspace(0.0, 1, samples)
+        t = np.linspace(class_number*4, (class_number+1)*4, samples) + np.random.randn(samples)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+
+X, y = create_data(samples=100, classes=3)
 
 # plt.scatter(X[:,0], X[:,1], c = y, cmap="brg")
 # plt.show()
