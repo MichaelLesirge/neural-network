2bfc378001ee7dd491d734d911a6a24aae7d08e8
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-07-28 14:21:14 -0700

    remove example code

--- neural_network/losses.py
+++ neural_network/losses.py
@@ -43,25 +43,7 @@ class CategoricalCrossentropy(Loss):
     _verbose_name = "categorical crossentropy"
     
     def loss(self, y_true, y_pred):
-        samples = len(y_pred)
-        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
-
-        # Probabilities for target values -
-        # only if categorical labels
-        if len(y_true.shape) == 1:
-            correct_confidences = y_pred_clipped[
-                range(samples),
-                y_true
-            ]
-        # Mask values - only for one-hot encoded labels
-        elif len(y_true.shape) == 2:
-            correct_confidences = np.sum(
-                y_pred_clipped * y_true,
-                axis=1
-            )
-
-        # Losses
-        negative_log_likelihoods = -np.log(correct_confidences)
-        return negative_log_likelihoods
-
+        pass
+    
     def loss_prime(y_true, y_pred):
+        pass
