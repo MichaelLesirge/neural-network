b79045032ad057b07cb7d9d088760a428d7331ea
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-08-07 19:24:19 -0700

    add loss base

--- neural_network/losses.py
+++ neural_network/losses.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import Any
 
 import numpy as np
 
@@ -9,4 +8,22 @@ from base import BaseLayer
 class Loss(BaseLayer, ABC):
     def __init__(self) -> None:
         super().__init__()
+ 
+    def forward(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        
+        if y_true.ndim == 1:
+            classes = np.size(y_pred, 1)
+            y_true = np.eye(classes)[range(classes), y_true]
+        
+        loss = self.loss(y_true, y_pred)
+        return np.mean(loss)
     
+    def backward(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        samples = np.size(y_true, 0)
+    
+    @abstractmethod
+    def loss(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        pass
+        
+    def loss_prime(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        pass
