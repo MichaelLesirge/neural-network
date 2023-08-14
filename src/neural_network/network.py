08834351864633d15b02e25a76b565e0103f04d0
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-08-14 01:19:45 -0700

    add preprocessing and improve logging

--- src/neural_network/network.py
+++ src/neural_network/network.py
@@ -20,32 +20,50 @@ def same_shuffle(*arrays):
     return tuple(array[order] for array in arrays)
 
 class Network:
-    def __init__(self, layers: list[BaseLayer], loss: Loss) -> None:
+    def __init__(self, layers: list[BaseLayer], loss: Loss, preprocess: list = []) -> None:
         self.layers = layers
         self.loss = loss
+        self.reprocesses = preprocess
     
     def compute(self, inputs: np.ndarray) -> np.ndarray:
+        
+        for proc in self.reprocesses:
+            inputs = proc(inputs)
+        
         for layer in self.layers:
             inputs = layer.forward(inputs)
         return inputs
     
     
     def train(self, x: np.ndarray, y: np.ndarray, learning_rate: float = 0.001, batch_size: int = 32, epochs: int = 1, shuffle: bool = True, is_categorical_labels: bool = False):
+
+        for proc in self.reprocesses:
+            x = proc(x)
+        
         for epoch in range(epochs):
             if shuffle:
                 x, y = same_shuffle(x, y)
-            for batch, (x_batch, y_batch) in enumerate(zip(n_split_array(x, batch_size, keep_extra=False), n_split_array(y, batch_size, keep_extra=False))):
+            x_split, y_split = n_split_array(x, batch_size, keep_extra=False), n_split_array(y, batch_size, keep_extra=False)
+            for batch, (x_batch, y_batch) in enumerate(zip(x_split, y_split)):
                 zs = [x_batch]
                 for layer in self.layers:
                     activation = layer.forward(zs[-1])
                     zs.append(activation)
                     
+                # todo add more logging options and make it so it ends at 100 and batch at 50 by +1
+                    
                 output = zs.pop()
                                 
                 loss = self.loss.forward(y_batch, output, is_categorical_labels = is_categorical_labels)
-                if batch % 100 == 0: print(f"{epoch=}, {batch=}, {loss=}")
+                
+                percent_complete = (batch + len(x_split) * epoch) / (len(x_split) * epochs) * 100
+                if percent_complete % 1 == 0: print(f"{int(percent_complete)}% complete. {epoch=}, {batch=}, {loss=}".ljust(30), end="\r")
                                    
                 grad = self.loss.backward(y_batch, output, is_categorical_labels = is_categorical_labels)
                 
                 for layer, activation in zip(reversed(self.layers), reversed(zs)):
                     grad = layer.backward(activation, grad, learning_rate)
+            
+            print(f"{int(percent_complete)}% complete. {epoch=}, {batch=}, {loss=}".ljust(20), end="\n")
+            
+        print(f"100% complete. {loss=}")
