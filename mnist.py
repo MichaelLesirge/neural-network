05d5f11b4b592c3e5114241bc88ddb7baf1cdd56
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-12-26 15:43:28 -0800

    fix issue with loading data. int8 vs uint8

--- mnist.py
+++ mnist.py
@@ -84,11 +84,11 @@ except FileNotFoundError:
     with open("ml_projects/mnist/mnist_train_test.pkl", "rb") as file:
         ((train_len, X_train, y_train), (test_len, X_test, y_test)) = pickle.load(file)
         
-        X_train = np.frombuffer(X_train, dtype=np.int8).reshape((train_len, small_drawing_height, small_drawing_width))
-        y_train = np.frombuffer(y_train, dtype=np.int8).reshape((train_len,))
+        X_train = np.frombuffer(X_train, dtype=np.uint8).reshape((train_len, small_drawing_height, small_drawing_width))
+        y_train = np.frombuffer(y_train, dtype=np.uint8).reshape((train_len,))
         
-        X_test = np.frombuffer(X_test, dtype=np.int8).reshape((test_len, small_drawing_height, small_drawing_width))
-        y_test = np.frombuffer(y_test, dtype=np.int8).reshape((test_len,))
+        X_test = np.frombuffer(X_test, dtype=np.uint8).reshape((test_len, small_drawing_height, small_drawing_width))
+        y_test = np.frombuffer(y_test, dtype=np.uint8).reshape((test_len,))
             
     print("Starting Training...")
     network.train(X_train, y_train, batch_size=16, epochs=2, learning_rate=0.1)
