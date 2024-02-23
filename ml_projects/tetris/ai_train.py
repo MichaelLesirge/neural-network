be6cf65e848b27caad91f527ad4d09d5b85896a3
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2024-02-23 03:05:04 -0800

    start new training run

--- ml_projects/tetris/ai_train.py
+++ ml_projects/tetris/ai_train.py
@@ -1,5 +1,3 @@
-from statistics import mean
-
 import numpy as np
 
 import constants
@@ -12,69 +10,59 @@ def main():
     
     env = Tetris(
         constants.BOARD_WIDTH, constants.BOARD_HEIGHT,
-        enable_wall_kick=False, piece_queue_size=False, enable_hold=False
+        enable_wall_kick=True, piece_queue_size=False, enable_hold=False
     )
     
-    episodes = 20000
-    epsilon_stop_episode = 15000
-    mem_size = 20000
-    discount = 0.95
+    episodes = 200_000
+    epsilon_stop_episode = episodes * 0.9
+
     max_steps = float("inf")
-    replay_start_size = 6000
 
     batch_size = 512
     epochs = 1
-    learning_rate = 0.01
     
     train_every = 1
 
     render_every = 100
     log_every = 100
 
+    # ai.load()
+    
     agent = DQNAgent(ai.network, ai.state_size,
-                     learning_rate=learning_rate,
+                     learning_rate=0.01,
                      epsilon_stop_episode=epsilon_stop_episode,
-                     mem_size=mem_size,
-                     discount=discount,
-                     replay_start_size=replay_start_size)
+                     mem_size=20_000,
+                     discount=0.99,
+                     replay_start_size=6000)
 
     scores = []
-    rewards = []
+    average_game_rewards = []
     
     move_counter = {move: 0 for move in ai.potential_moves}
 
     for episode in range(episodes):
         env.reset()
-        
-        current_state = env.game_to_inputs()
-        
+                
         done = False
         steps = 0 
-
+        
+        game_rewards = []
         # Game
         while (not done) and (steps < max_steps):
             next_states = env.get_next_states(ai.potential_moves)
             
-            best_state = agent.best_state(next_states.values())
-            
-            # reverse lookup on dict
-            best_action = None
-            for action, state in next_states.items():
-                if np.array_equal(state, best_state):
-                    best_action = action
-                    break
+            best_action = agent.best_action(next_states)
 
             move_counter[best_action] += 1
             state, reward, done, info = env.step([best_action])
                         
-            agent.add_to_memory(current_state, reward, done, next_states[best_action])
-            
-            current_state = next_states[best_action]
-            
+            agent.add_to_memory(state, reward, done, next_states[best_action])
+                        
+            game_rewards.append(reward)
             steps += 1
 
+        average_game_rewards.append(np.mean(game_rewards))
         scores.append(info["score"])
-        rewards.append(reward)
 
         # Train
         if episode % train_every == 0:
@@ -82,34 +70,37 @@ def main():
 
         # Render
         if render_every and episode % render_every == 0:
+            print(info)
             print(env.render_as_str())
 
         # Logs
         if episode % log_every == 0:
-            print(f"Episode #{episode} ({episode / episodes:.1%})")
+            print(f"Episode #{episode} ({episode / episodes:.1%}). {agent.epsilon = }")
 
-            avg_score = mean(scores[-log_every:])
+            avg_score = np.mean(scores[-log_every:])
             min_score = min(scores[-log_every:])
             max_score = max(scores[-log_every:])
 
             print(f"{avg_score = }, {min_score = }, {max_score = }")
 
-            avg_reward = mean(rewards[-log_every:])
-            min_reward = min(rewards[-log_every:])
-            max_reward = max(rewards[-log_every:])
+            avg_reward = np.mean(average_game_rewards[-log_every:])
+            min_reward = min(average_game_rewards[-log_every:])
+            max_reward = max(average_game_rewards[-log_every:])
 
             print(f"{avg_reward = }, {min_reward = }, {max_reward = }")
 
-            avg_score = mean(scores)
-            avg_reward = mean(rewards)
+            avg_score = np.mean(scores)
+            avg_reward = np.mean(average_game_rewards)
             print(f"Overall: {avg_score = }, {avg_reward = }")
             
             print({move.name if move else "NONE": count for (move, count) in move_counter.items()})
             move_counter = {move: 0 for move in move_counter}
             
             print()
-    
-    print(f"Episode #{episode} (100%): {avg_score=}, {min_score=}, {max_score=}\n")
+            
+    avg_score = np.mean(scores)
+    avg_reward = np.mean(average_game_rewards)    
+    print(f"Final Overall: {avg_score = }, {avg_reward = }")
     
     ai.save()
 
