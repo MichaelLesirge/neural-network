05de340867e3334c604eb306b391ef7e93777bbe
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2025-05-08 17:36:04 -0700

    fix activation graph testing program

--- neural_network/activations/graph.py
+++ neural_network/activations/graph.py
@@ -1,32 +1,34 @@
+import pathlib
+import sys
+
+directory = pathlib.Path(__file__).parent.absolute()
+sys.path.append(str(directory.parent.parent))
+
 from activations import *
 from matplotlib import pyplot as plt
 
+def main():
+    all = [BinaryStep(), Sigmoid(), HardSigmoid(), Tanh(), Affine(), Linear(), Exponential(), ReLU(), LeakyReLU(), ELU(), GELU(), SELU(), Swish(), Softplus(), Softmax()]
+    common = [Sigmoid(), Tanh(), ReLU(), LeakyReLU(), ELU(), GELU(), Swish()]
 
-def graph_all(x: np.ndarray = None) -> None:
-    funcs: list[Activation] = [BinaryStep, Sigmoid, HardSigmoid, Tanh, Affine, Linear,
-                               Exponential, ReLU, LeakyReLU, ELU, GELU, SELU, Swish, Softplus, Softmax]
+    x= np.linspace(-1, 1, 100)
 
-    for activation in funcs:
-        graph_activation(activation, x)
+    for activation in common:
+        y = activation.activation(x)
+        m = activation.activation_prime(x)
 
+        plt.title(repr(activation))
+        plt.axhline(0, color='dimgrey', linewidth=1)
+        plt.axvline(0, color='dimgrey', linewidth=1)
 
-def graph_activation(activation: Activation, x: np.ndarray = None):
+        plt.xlim(-1, 1)
+        plt.ylim(-1, 1)
 
-    if x is None: x = np.arange(-5, 5, 0.01)
-    if x.ndim == 1: x = x.reshape((1, ) + x.shape)
+        plt.plot(x, m, label="activation prime", linestyle="--")
+        plt.plot(x, y, label="activation")
 
-    y = activation(x)
+        plt.legend(loc="best")
+        plt.show()
 
-    plt.title(repr(activation))
-    plt.axhline(0, color='dimgrey', linewidth=1)
-    plt.axvline(0, color='dimgrey', linewidth=1)
-    # plt.ylim(-1.2, 1.2)
-
-    plt.plot(x, y, label="activation")
-    plt.plot(x, activation.activation_prime(x), label="activation prime")
-
-    print(activation)
-
-    plt.legend(loc="best")
-    plt.grid(True)
-    plt.show()
+if __name__ == "__main__":
+    main()
